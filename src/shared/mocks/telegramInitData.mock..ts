ab8a0c9b896aeecff0766499cf51ff72286b119f
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-04 15:43:48 +0300

    feat: create telegram initial data mock

---
+++ src/shared/mocks/telegramInitData.mock..ts
@@ -0,0 +1,15 @@
+import { IWebAppInitData } from 'shared/types';
+
+export const telegramInitDataMock: IWebAppInitData = {
+  query_id: 'jU$(OHHP(43j389j9fOHUFEO',
+  user: {
+    id: 344323443,
+    first_name: 'TestUser',
+    last_name: '',
+    username: 'TestUser',
+    language_code: 'ru',
+    allows_write_to_pm: true,
+  },
+  auth_date: 1699101655,
+  hash: '5j4ttt8o7wtdk5thkojw54tlp5843795u345y3xhy3k843hrodjl9k48h84h85h3',
+};
