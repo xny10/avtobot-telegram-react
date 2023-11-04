4dbb65f746e54f7c4db94f90d0804f87daf9adcd
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-04 15:58:27 +0300

    chore: delete theme provider
    cause its auto-provided by telegram

--- src/app/providers/ThemeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider as MUIThemeProvider } from '@mui/material';
-import { PropsWithChildren } from 'react';
-import { theme } from 'shared/config/theme';
-
-type ThemeProviderProps = PropsWithChildren;
-
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  return <MUIThemeProvider theme={theme}>{children}</MUIThemeProvider>;
-}
