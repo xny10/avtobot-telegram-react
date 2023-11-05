ac85035e55698a06ba21b6c9f4cc4b995b8a8258
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-05 19:30:24 +0300

    feat: add switch to layout

--- src/widgets/filters-list/ui/FiltersList.tsx
+++ src/widgets/filters-list/ui/FiltersList.tsx
@@ -1,4 +1,5 @@
 import { FilterCard } from 'entities/filter';
+import { SwitchFilter } from 'features/filter';
 import { IFilterShort } from 'shared/types';
 
 import styles from './styles.module.scss';
@@ -11,7 +12,12 @@ export function FiltersList({ filters }: FiltersListProps) {
   return (
     <div className={styles.list}>
       {filters.map((filter, i) => (
-        <FilterCard key={filter.id} filter={filter} index={i} />
+        <FilterCard
+          key={filter.id}
+          filter={filter}
+          index={i}
+          buttonSlot={<SwitchFilter filterId={filter.id} isActive={filter.isActive} />}
+        />
       ))}
     </div>
   );
