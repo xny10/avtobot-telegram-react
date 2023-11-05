090a9ea1badc09fdebc6cfe92770252f0d3819c9
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-05 16:39:34 +0300

    feat: add different behavior for "go back" link

--- src/ui/base-layout/BaseLayout.tsx
+++ src/ui/base-layout/BaseLayout.tsx
@@ -1,19 +1,23 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { IconButton, Typography } from '@mui/material';
 import { PropsWithChildren } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTelegram } from 'shared/hooks/useTelegram';
 
 import styles from './styles.module.scss';
 
 type BaseLayoutProps = PropsWithChildren<{
   title: string;
+  backLinkBehavior?: 'previous_page' | 'exit_telegram';
 }>;
 
-export function BaseLayout({ children, title }: BaseLayoutProps) {
+export function BaseLayout({ children, title, backLinkBehavior = 'exit_telegram' }: BaseLayoutProps) {
   const { tg } = useTelegram();
+  const navigate = useNavigate();
 
   const onClose = () => {
-    tg.close();
+    if (backLinkBehavior === 'exit_telegram') tg.close();
+    else navigate(-1);
   };
 
   return (
