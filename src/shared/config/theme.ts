8c6dd5fd13f2f0861816ee7d34f8b05bed98f400
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-05 16:20:35 +0300

    feat: upd theme

--- src/shared/config/theme.ts
+++ src/shared/config/theme.ts
@@ -1,3 +1,18 @@
 import { createTheme } from '@mui/material';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  typography: {
+    allVariants: {
+      color: 'var(--tg-theme-text-color)',
+    },
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          background: 'var(--tg-theme-hint-color)',
+        },
+      },
+    },
+  },
+});
