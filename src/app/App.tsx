143a6e9b96b38fb42adc8c02111aa5f07de81d33
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-05 21:09:38 +0300

    feat: prevent collapse of telegram iframe

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,6 +17,17 @@ export function App() {
     tg.HapticFeedback.impactOccurred('soft');
   }, []);
 
+  useEffect(() => {
+    // prevent telegram iframe акщь collapsing
+    function handler(event: TouchEvent) {
+      event.preventDefault();
+    }
+    document.addEventListener('touchmove', handler, { passive: false });
+    return () => {
+      document.removeEventListener('touchmove', handler);
+    };
+  }, []);
+
   return (
     <ReactQueryProvider>
       <StyledEngineProvider injectFirst>
