20e8361f08c9639ba50b0830d8b7aa31914c9422
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-05 16:21:05 +0300

    ref: extract routes

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,12 +1,13 @@
 import { ConfiguratorPage } from 'pages/configurator-page';
 import { HomePage } from 'pages/home-page';
 import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
 
 export function Router() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/configure" element={<ConfiguratorPage />} />
+      <Route path={`${ROUTES.filter}/:id`} element={<ConfiguratorPage />} />
     </Routes>
   );
 }
