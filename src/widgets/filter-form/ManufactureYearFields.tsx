fe825a0a2c53c354f58ceb8401443b9423fc0f2f
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2024-01-11 13:53:55 +0300

    feat: create validator

--- src/widgets/filter-form/ManufactureYearFields.tsx
+++ src/widgets/filter-form/ManufactureYearFields.tsx
@@ -1,7 +1,7 @@
 import { MenuItem } from '@mui/material';
 import { RangedInput } from 'features/filter';
-import { memo } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { memo, useEffect } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 import { RHFSelect } from 'ui/react-hook-form';
 
 import styles from './styles.module.scss';
@@ -24,6 +24,28 @@ export const ManufactureYearFields = memo(function ManufactureYearFields({ optio
     setValue('manufactureYear.1', '', { shouldDirty: true });
   };
 
+  const valueLeft = useWatch({ name: 'manufactureYear.0' });
+  const valueRight = useWatch({ name: 'manufactureYear.1' });
+
+  const indexLeft = options.indexOf(valueLeft);
+  const indexRight = options.indexOf(valueRight);
+
+  const isValid = indexLeft >= indexRight;
+
+  useEffect(() => {
+    if (!isValid) {
+      setValue('manufactureYear.1', '', { shouldDirty: true });
+    }
+  }, [isValid]);
+
+  let availableOptionsRight;
+  if (valueLeft === '') {
+    availableOptionsRight = options;
+  } else {
+    const index = options.indexOf(valueLeft);
+    availableOptionsRight = options.slice(0, index + 1);
+  }
+
   return (
     <RangedInput
       title="Год выпуска"
@@ -45,7 +67,7 @@ export const ManufactureYearFields = memo(function ManufactureYearFields({ optio
       }
       RightInput={
         <RHFSelect
-          options={options}
+          options={availableOptionsRight}
           control={control}
           name="manufactureYear.1"
           label="до"
