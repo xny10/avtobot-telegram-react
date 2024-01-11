83924c46ddaa1b499018d9264f3af39212036a94
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2024-01-11 11:26:57 +0300

    Update buildWebpack.ts

--- config/buildWebpack.ts
+++ config/buildWebpack.ts
@@ -17,6 +17,7 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
       path: paths.output,
       filename: '[name].[contenthash].js',
       clean: true,
+      publicPath: '/',
     },
     plugins: buildPlugins(options),
     module: {
