fdbecac063b43bbba3ede369a4431644195ee8c8
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2024-01-13 15:50:07 +0300

    fix: make clear button clear everything instead choosing everything

--- src/widgets/filter-form/utils/clearLocation.ts
+++ src/widgets/filter-form/utils/clearLocation.ts
@@ -2,16 +2,12 @@ import { FieldValues, UseFormSetValue } from 'react-hook-form';
 import { ISearchType } from 'shared/types';
 
 export function clearLocation(searchType: ISearchType, setValue: UseFormSetValue<FieldValues>) {
-  // TODO: должны браться из кэша rtkq
-  const cityOptions = ['Ростов-на-Дону', 'Москва', 'Таганрог'];
-  const regionOptions = ['Ростовская обл.', 'Москва', 'Подмосковье', 'Адыгея', 'Тюменская обл.'];
-
   function clearRegion() {
-    setValue('region', regionOptions);
+    setValue('region', []);
   }
 
   function clearCity() {
-    setValue('city', cityOptions);
+    setValue('city', []);
   }
   if (searchType === 'region') clearRegion();
   else if (searchType === 'city') clearCity();
