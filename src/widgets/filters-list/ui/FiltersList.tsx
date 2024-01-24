5adf6a3001b41e44687bea5e813b586c94ef71d7
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2024-01-24 10:50:38 +0300

    feat: create button to navigate to create form

--- src/widgets/filters-list/ui/FiltersList.tsx
+++ src/widgets/filters-list/ui/FiltersList.tsx
@@ -1,6 +1,8 @@
+import { Button } from '@mui/material';
 import { FilterCard } from 'entities/filter';
-import { CopyFilter, SwitchFilter } from 'features/filter';
-import { DeleteFilter } from 'features/filter';
+import { CopyFilter, DeleteFilter, SwitchFilter } from 'features/filter';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
 import { IFilter } from 'shared/types';
 
 import styles from './styles.module.scss';
@@ -10,22 +12,33 @@ type FiltersListProps = {
 };
 
 export function FiltersList({ filters }: FiltersListProps) {
+  const navigate = useNavigate();
+
+  const onCreateNewFilter = () => {
+    navigate(ROUTES.createFilter);
+  };
+
   return (
-    <div className={styles.list}>
-      {filters.map((filter, i) => (
-        <FilterCard
-          key={filter.id}
-          filter={filter}
-          index={i}
-          Buttons={
-            <div>
-              <SwitchFilter filterId={filter.id} isActive={filter.isActive} />
-              <CopyFilter filterId={filter.id} />
-              <DeleteFilter filterId={filter.id} />
-            </div>
-          }
-        />
-      ))}
+    <div className={styles.layout}>
+      <Button variant="outlined" onClick={onCreateNewFilter}>
+        Создать новый
+      </Button>
+      <div className={styles.list}>
+        {filters.map((filter, i) => (
+          <FilterCard
+            key={filter.id}
+            filter={filter}
+            index={i}
+            Buttons={
+              <div>
+                <SwitchFilter filterId={filter.id} isActive={filter.isActive} />
+                <CopyFilter filterId={filter.id} />
+                <DeleteFilter filterId={filter.id} />
+              </div>
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 }
